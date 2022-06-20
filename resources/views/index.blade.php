8a8887160c14e9a7cdfa1b3dc5b892d0c30a63f8
Shubham Sareliya <sareliashubham9999@gmail.com>
2022-06-20 18:06:40 +0530

    Updated Home Page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -88,25 +88,21 @@
         </div>
         <div class="row px-xl-5 pb-3">
             @foreach ($variants->sortByDesc('id')->take(8) as $variant)
-            <a href="/shop_detail/{{$products->where('id', $variant->product_id)->first()->slug}}">
                 <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
                     <div class="card product-item border-0 mb-4">
                         <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-                            <img class="img-fluid w-100" src="{{ asset('uploads/variant/'.$variant->image) }}" alt="">
+                            <a href="/shop_detail/{{$products->where('id', $variant->product_id)->first()->slug}}">
+                                <img class="img-fluid w-100" src="{{ asset('uploads/variant/'.$variant->image) }}" alt="">
+                            </a>
                         </div>
                         <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
-                            <h6 class="text-truncate mb-3">{{ $variant->name }}</h6>
+                            <h6 class="text-truncate mb-3">{{ $variant->product->name }}</h6>
                             <div class="d-flex justify-content-center">
                                 <h6>$ {{ $variant->price }}</h6><h6 class="text-muted ml-2"><del>$ {{ $variant->price }}</del></h6>
                             </div>
                         </div>
-                        <div class="card-footer d-flex justify-content-between bg-light border">
-                            <a href="" class="btn btn-sm text-dark p-0"><i class="fas fa-eye text-primary mr-1"></i>View Detail</a>
-                            <a href="" class="btn btn-sm text-dark p-0"><i class="fas fa-shopping-cart text-primary mr-1"></i>Add To Cart</a>
-                        </div>
                     </div>
                 </div>
-            </a>
             @endforeach
         </div>
     </div>
